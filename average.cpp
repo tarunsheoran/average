a59a457b83933e55740f5d8640e418e83e786513
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-08-01 21:03:21 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a,b,c,d,e,sum,avg;
+    cout<<"Enter the numbers";
+    cin>>a>>b>>c>>d>>e;
+    sum=a+b+c+d+e;
+    cout<<sum;
+    avg=sum/5;
+    cout<<avg;
+    return 0;
+
+}
